54c47c05fbf14bf5eed18ab37889c0f8490bc47a
unknown <Anton Sherstiuk>
2021-03-02 14:27:58 +0100

    Requirements, initialization updates

--- init.sql
+++ init.sql
@@ -6,14 +6,27 @@ CREATE TABLE IF NOT EXISTS public."Tweets"
     original_id bigint,
     text character varying(500) COLLATE pg_catalog."default",
     lang character varying(10) COLLATE pg_catalog."default",
-    reply_count integer,
-    retweet_count integer,
-    favorite_count integer,
+--    reply_count integer,
+--    retweet_count integer,
+--    favorite_count integer,
     created_at date,
     user_verified boolean,
     user_followers integer,
 --    link character varying(200) COLLATE pg_catalog."default",
     factchecked boolean,
     sentiment_label integer, --character varying(10) COLLATE pg_catalog."default",
-    sentiment_score double precision
+    sentiment_score double precision,
+    keywords character varying[] 
 );
+
+CREATE VIEW positives AS
+SELECT id, original_id, text, lang, created_at, user_verified, user_followers, factchecked, sentiment_label, sentiment_score, keywords
+	FROM public."Tweets"
+	WHERE sentiment_label=1
+	ORDER BY round( CAST(sentiment_score as numeric), 3)  DESC, (CASE when user_verified then 1 else 2 end) ASC, user_followers DESC, factchecked;
+
+CREATE VIEW negatives AS
+SELECT id, original_id, text, lang, created_at, user_verified, user_followers, factchecked, sentiment_label, sentiment_score, keywords
+	FROM public."Tweets"
+	WHERE sentiment_label=0
+	ORDER BY round( CAST(sentiment_score as numeric), 3)  DESC, (CASE when user_verified then 1 else 2 end) ASC, user_followers DESC, factchecked;
