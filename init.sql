158925bf92e43464006e758f500e082cd4a54b47
unknown <Anton Sherstiuk>
2021-03-12 18:03:06 +0100

    Update to views in init.sql

--- init.sql
+++ init.sql
@@ -14,19 +14,17 @@ CREATE TABLE IF NOT EXISTS public."Tweets"
     user_followers integer,
 --    link character varying(200) COLLATE pg_catalog."default",
     factchecked boolean,
-    sentiment_label integer, --character varying(10) COLLATE pg_catalog."default",
-    sentiment_score double precision,
+    positive_score double precision,
+    negative_score double precision,
     keywords character varying[] 
 );
 
 CREATE VIEW positives AS
-SELECT id, original_id, text, lang, created_at, user_verified, user_followers, factchecked, sentiment_label, sentiment_score, keywords
+SELECT id, original_id, text, lang, created_at, user_verified, user_followers, factchecked, positive_score, negative_score, keywords
 	FROM public."Tweets"
-	WHERE sentiment_label=1
-	ORDER BY round( CAST(sentiment_score as numeric), 3)  DESC, (CASE when user_verified then 1 else 2 end) ASC, user_followers DESC, factchecked;
+	ORDER BY round( CAST(positive_score as numeric), 4)  DESC, (CASE when user_verified then 1 else 2 end) ASC, user_followers DESC, factchecked;
 
 CREATE VIEW negatives AS
-SELECT id, original_id, text, lang, created_at, user_verified, user_followers, factchecked, sentiment_label, sentiment_score, keywords
+SELECT id, original_id, text, lang, created_at, user_verified, user_followers, factchecked, positive_score, negative_score, keywords
 	FROM public."Tweets"
-	WHERE sentiment_label=0
-	ORDER BY round( CAST(sentiment_score as numeric), 3)  DESC, (CASE when user_verified then 1 else 2 end) ASC, user_followers DESC, factchecked;
+	ORDER BY round( CAST(negative_score as numeric), 4)  DESC, (CASE when user_verified then 1 else 2 end) ASC, user_followers DESC, factchecked;
